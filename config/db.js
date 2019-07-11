8fad011d3d3d6c1a588169855fccddb58beacaae
edb-c <edb.cofresi@gmail.com>
2019-07-11 00:38:16 -0400

    Connect to MongoDB with Mongoose

---
+++ config/db.js
@@ -0,0 +1,25 @@
+//Mongodb Connection
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+//Use async/await - asynchronous arrow function
+//Use try/catch block, so if it fails, the error message will display
+//mongoose.connect returns a promise so we put await before it
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,  
+
+    });
+
+    console.log('Successfully Connected to MongoDB');
+  } catch (err) {
+    console.error(err.message);
+    // Escape process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB; //export the function
