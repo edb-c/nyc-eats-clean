5b98faf8c301ea3dc4e665524609ef6b6c55e7b1
edb-c <edb.cofresi@gmail.com>
2019-07-11 10:18:27 -0400

    Create Landing Page

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+
+        <div className='landing-inner'>
+          <h1 className='large'>Eat Clean In</h1>
+        </div>
+
+    </section>
+  );
+};
+
+export default Landing;
