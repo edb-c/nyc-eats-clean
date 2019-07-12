438c9bc2bdd02e18d44bb68b22c3ef1aef824fe8
edb-c <edb.cofresi@gmail.com>
2019-07-11 22:35:10 -0400

    Create Fetch NYC Health Inspection Results

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,7 +7,10 @@ const Landing = () => {
         <div className='landing-inner'>
           <h1 className='large'>Eat Clean In</h1>
          </div>
-         <p className='text-center'> Made with <span className='hearts'>&hearts;</span>by E.Cofresi 2019</p>
+         <p className='text-center'> 
+         Find the New York City Health Inspection Grades of the eateries near you. Create an Account to save your favorite places!
+         <br />
+         Made with <span className='hearts'>&hearts;</span> by <a href='http://the.ethelcofresi.com/'>E.Cofresi 2019</a></p>
 
      </section>
   );
--- client/src/containers/Eateries.js
+++ client/src/containers/Eateries.js
@@ -1,22 +1,63 @@
 import React, { Component, Fragment } from "react";
 import GoogleMapContainer from './GoogleMapContainer';
+import CardGroup from 'react-bootstrap/CardGroup'
+import Card from 'react-bootstrap/Card'
 
-import '../assets/css.css';
-
+export const Grades = ({grades}) => {
+ return (
+  <div>
+      <center><h2>NYC Restaurant Inspection Results</h2></center>
+      {grades.map((grade) => (
+        <Fragment>
+          <CardGroup>
+  <Card>
+  <Card.Body>
+  <Card.Title>{grade.dba} - Grade: {grade.grade} </Card.Title>
+  <Card.Text>
+            Cuisine: {grade.cuisine_description} <br />
+            Address: {grade.building} {grade.street}, {grade.boro} {grade.zipcode} <br />
+            Phone: {grade.phone}  <br />
+            Violation Description: {grade.violation_description} <br />
+              </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+      <small className="text-muted">Last updated: {grade.grade_date}</small>
+    </Card.Footer>
+          </Card>
+          </CardGroup>
+        </Fragment>        
+      ))}
+  </div>
+)
+};
+  
 class Eateries extends Component {
+  componentDidMount() {
+    fetch('https://data.cityofnewyork.us/resource/43nn-pn8j.json')
+          .then(res => res.json())
+          .then((data) => {
+              this.setState({ grades: data })
+          })
+          .catch(console.log)
+  } //end componentDidMount
+
+  //A state = object that holds data pending to be rendered. Will store the output from the API call.
+  state = {
+    grades: []
+  };
 
   render() {
       return (  
-        <div class="row">
-          <div className="column" >
-            <h2>Column 1</h2>
-            <p>Some text..</p>
+        <Fragment>
+          <div class="row">
+            <div className="column" >
+              <Grades grades={this.state.grades} />
+            </div>
+            <div className="column" >
+              <GoogleMapContainer />
+            </div>
           </div>
-          <div className="column" >
-            <GoogleMapContainer />
-          </div>
-        </div>
- 
+        </Fragment>
       ) //end return
   } //end render
 } //end class Eateries
