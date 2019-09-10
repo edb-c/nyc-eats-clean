23a3dee69597564bf2a93be08a5aef54ad9d0d20
edb-c <edb.cofresi@gmail.com>
2019-09-10 12:55:10 -0400

    Set-up Create Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -12,8 +13,7 @@ const User = require('../../models/User');
 router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id }).populate(
-      'user'
-      //   ['name', 'avatar']
+      'user', 'name'
     );
 
     if (!profile) {
@@ -27,4 +27,58 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
-module.exports = router;
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+    '/', 
+    
+    [
+        auth, //middleware
+        [
+        check('', "Is required")
+        ]
+    ],     
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const {
+     //tdb
+      } = req.body;
+  
+      // Build profile object
+      const profileFields = {};
+      profileFields.user = req.user.id;
+     
+      try {
+        let profile = await Profile.findOne({ user: req.user.id });
+  
+        if (profile) {
+          // Update
+          profile = await Profile.findOneAndUpdate(
+            
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true }
+          );
+  
+          return res.json(profile);
+        }
+  
+        // Create
+        profile = new Profile(profileFields);
+  
+        await profile.save();
+        res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    }
+  );
+  
+  module.exports = router;
+
