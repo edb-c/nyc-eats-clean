17b0a7436b9b40c6499b51d832c866404f643462
edb-c <edb.cofresi@gmail.com>
2019-09-10 10:57:23 -0400

    JWT related - middleware and verify

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,20 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
+//This is a middleare function, which has access to the request/response cycle, 
+//next is a callback to run once done, so it moves on to the next piece of middleware
+
 module.exports = function(req, res, next) {
     
-  // Get token from header
+// Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if not token
+// Check if not token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
-  // Verify token
+// Verify token
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,8 +9,8 @@ const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 
 // @route    GET api/auth
-// @desc     Test route
 // @access   Public
+//Making call to our database, leaving out password
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
