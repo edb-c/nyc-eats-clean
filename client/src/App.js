03b643764238642a4a6c2a852635a85d3d372caf
edb-c <edb.cofresi@gmail.com>
2019-09-24 00:10:16 -0400

    Add Profile action/reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React, {Fragment, useEffect } from "react";
+import React, { Fragment, useEffect } from 'react';
 //Switch Component groups <Route>s together
-import { Switch, Route, withRouter } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
 import Login from './components/auth/Login';
 import EateriesComponent from './containers/EateriesContainer';
+
 import './assets/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,28 +22,27 @@ import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
-};
+}
 
 //React Function using Hooks
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
-  }, []);  //Empty arry bracket makes this run only once, not in a loop
- 
-    return (
-      <Fragment>
-        <Navbar />
-        <Alert />
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/eateries' component={EateriesComponent} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />    
-        </Switch>
-       
-      </Fragment>
-    );
+  }, []); //Empty arry bracket makes this run only once, not in a loop
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Alert />
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/eateries' component={EateriesComponent} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </Fragment>
+  );
 }; //end class App
 
 export default withRouter(App);
-
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile } from '../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth, profile }) => {
   useEffect(() => {
