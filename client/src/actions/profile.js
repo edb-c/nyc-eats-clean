6623f4ec478b6798e28d838157579b277e62dd45
edb-c <edb.cofresi@gmail.com>
2019-09-24 12:36:27 -0400

    Add Create Profile Component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,6 +19,49 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,  //has method called push
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+
+
+
 // Delete account & profile
 export const deleteAccount = () => async dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
--- client/src/components/CreateProfile.js
+++ client/src/components/CreateProfile.js
@@ -1,26 +1,30 @@
 import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     bio: ''
   });
 
-  const {
-    bio,
-  } = formData;
+  const { bio } = formData;
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
       <p className='lead'>
         <i className='fas fa-user' /> Share A Little About Yourself!
       </p>
-      <form className='form'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <textarea
             placeholder='A short bio of yourself'
@@ -30,18 +34,19 @@ const CreateProfile = props => {
           />
         </div>
 
-  
-
-      
         <input type='submit' className='btn btn-primary my-1' />
         <a className='btn btn-light my-1' href='dashboard.html'>
           Go Back
         </a>
-        </form>
+      </form>
     </Fragment>
   );
 };
 
-CreateProfile.propTypes = {};
-
-export default CreateProfile;
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
+export default connect(
+  null,
+  { createProfile }
+)(withRouter(CreateProfile));
